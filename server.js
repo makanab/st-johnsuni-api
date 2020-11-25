625f76005836dbf46da82327e6cfcd6a31995a4b
makanab <makanabrian1@gmail.com>
2020-11-25 22:03:26 +0300

    env

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
@@ -9,7 +11,7 @@ app.use('/api',rts)
 
 
 
-app.listen(5000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log('server started ....');
 });
 

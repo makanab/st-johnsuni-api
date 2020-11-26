eb0c5da6b0ac91a260d4f5798f58c0b943eecfbd
makanab <makanabrian1@gmail.com>
2020-11-26 08:51:13 +0300

    cors

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
+const bodypaser = require('body-parser');
 const app = express();
-
-
 const rts = require('./routes/index.route');
 
+app.use(bodypaser.json());
+app.use(bodypaser.urlencoded({extended:true}));
+app.use(cors());
 app.use('/api',rts)
 
 
